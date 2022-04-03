fafe2ceee59bf7a8b11e106aeb2ec29bb6963f9d
Connor Thompson <Ct6355@gmail.com>
2022-04-03 22:18:26 +0100

    added menu screens and fade between each
    menu scene commit

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+
+    
+    public Canvas c;
+    public Canvas cTurnoff;
+   // public GameObject logoHolder;
+    public string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+
+
+    public void ButtonFire()
+    {
+        Debug.Log("Testing");
+    
+        StartCoroutine(FadeOut()); // fade out old canvas
+      
+    }
+
+
+    IEnumerator FadeOut()
+    {
+
+        for (int i = 0; i != 20; i++)
+        {
+            var alphaCurrent = cTurnoff.GetComponent<CanvasGroup>().alpha;
+            Debug.Log("Fading");
+            cTurnoff.GetComponent<CanvasGroup>().alpha = alphaCurrent - 0.05f;
+            yield return new WaitForSeconds(0.01f);
+        }
+
+     
+        c.enabled = true;
+    }
+
+    public void LoadNewScene()
+    { 
+        StartCoroutine(StartingScene());
+    }
+
+
+    // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+
+    IEnumerator StartingScene()
+    {
+        if(SceneName == "CTBossFight")
+        {
+            yield return new WaitForSeconds(1.6f);
+        }
+        
+        SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+    }
+}
