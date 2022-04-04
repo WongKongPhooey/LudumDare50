13dbe22aab077d0ea3a9387b6b64d287f3695ace
Connor Thompson <Ct6355@gmail.com>
2022-04-04 09:41:32 +0100

    success / fail ui
    add game over  / game win ui

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,9 +15,15 @@ public class GameManager : MonoBehaviour
 	public static int currentCombo;
 	
 	public static int health;
-	
+
+
+    public GameObject gameOverUI;
+    public GameObject winUI;
+    public bool BossDead = false;
+
     // Start is called before the first frame update
     void Start(){
+       EndGame(); //Endgame needs to be triggered to ping up correct ui
     }
 
     // Update is called once per frame
@@ -24,4 +31,21 @@ public class GameManager : MonoBehaviour
        Text scoreText = scoreUI.GetComponent<Text>();
 	   scoreText.text = "Score: " + notesHit + " Combo: " + currentCombo;
     }
+
+    void EndGame()
+    {
+        Time.timeScale = 0f;
+        if( BossDead == false)
+        {
+            Instantiate(gameOverUI, transform.position, transform.rotation);
+        }
+        else
+        {
+            Instantiate(winUI, transform.position, transform.rotation);
+        }
+        
+
+    }
+
+    
 }
---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+  //  public string SceneName;
+    // Start is called before the first frame update
+   
+    public void RestartLevel()
+    {
+        Debug.Log("Restarting");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+}
